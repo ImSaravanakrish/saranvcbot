711ef1671e69a3a25f95195164b633fb936781ae
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2021-07-13 16:25:04 +0530

    Update userbotjoin.py

--- VCsMusicBot/modules/userbotjoin.py
+++ VCsMusicBot/modules/userbotjoin.py
@@ -34,7 +34,7 @@ async def addchannel(client, message):
         print(e)
         await message.reply_text(
             f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your group due to heavy join requests for userbot! Make sure user is not banned in group."
-            "\n\nOr manually add @VCsMusicPlayer to your Group and try again</b>",
+            "\n\nOr manually add music assistant to your Group and try again</b>",
         )
         return
     await message.reply_text(
@@ -109,7 +109,7 @@ async def addcchannel(client, message):
         print(e)
         await message.reply_text(
             f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your channel due to heavy join requests for userbot! Make sure user is not banned in channel."
-            "\n\nOr manually add @VCsMusicPlayer to your Group and try again</b>",
+            "\n\nOr manually add music assistant to your Group and try again</b>",
         )
         return
     await message.reply_text(
